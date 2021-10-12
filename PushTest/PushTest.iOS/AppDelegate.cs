81a8ab58f31f15f545843fe10f081def9d59a7e1
Edwin Arevalo <edwin.arevalo@tecsup.edu.pe>
2021-10-12 16:44:31 -0500

    test with api published

--- PushTest/PushTest.iOS/AppDelegate.cs
+++ PushTest/PushTest.iOS/AppDelegate.cs
@@ -5,6 +5,14 @@ using System.Linq;
 using Foundation;
 using UIKit;
 
+using System.Diagnostics;
+using System.Threading.Tasks;
+using PushTest.iOS.Extensions;
+using PushTest.iOS.Services;
+using PushTest.Services;
+using UserNotifications;
+using Xamarin.Essentials;
+
 namespace PushTest.iOS
 {
     // The UIApplicationDelegate for the application. This class is responsible for launching the 
@@ -13,6 +21,81 @@ namespace PushTest.iOS
     [Register("AppDelegate")]
     public partial class AppDelegate : global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate
     {
+
+        IPushDemoNotificationActionService _notificationActionService;
+        INotificationRegistrationService _notificationRegistrationService;
+        IDeviceInstallationService _deviceInstallationService;
+
+        IPushDemoNotificationActionService NotificationActionService
+            => _notificationActionService ??
+                (_notificationActionService =
+                ServiceContainer.Resolve<IPushDemoNotificationActionService>());
+
+        INotificationRegistrationService NotificationRegistrationService
+            => _notificationRegistrationService ??
+                (_notificationRegistrationService =
+                ServiceContainer.Resolve<INotificationRegistrationService>());
+
+        IDeviceInstallationService DeviceInstallationService
+            => _deviceInstallationService ??
+                (_deviceInstallationService =
+                ServiceContainer.Resolve<IDeviceInstallationService>());
+
+        void RegisterForRemoteNotifications()
+        {
+            MainThread.BeginInvokeOnMainThread(() =>
+            {
+                var pushSettings = UIUserNotificationSettings.GetSettingsForTypes(
+                    UIUserNotificationType.Alert |
+                    UIUserNotificationType.Badge |
+                    UIUserNotificationType.Sound,
+                    new NSSet());
+
+                UIApplication.SharedApplication.RegisterUserNotificationSettings(pushSettings);
+                UIApplication.SharedApplication.RegisterForRemoteNotifications();
+            });
+        }
+
+        Task CompleteRegistrationAsync(NSData deviceToken)
+        {
+            DeviceInstallationService.Token = deviceToken.ToHexString();
+            return NotificationRegistrationService.RefreshRegistrationAsync();
+        }
+
+        void ProcessNotificationActions(NSDictionary userInfo)
+        {
+            if (userInfo == null)
+                return;
+
+            try
+            {
+                var actionValue = userInfo.ObjectForKey(new NSString("action")) as NSString;
+
+                if (!string.IsNullOrWhiteSpace(actionValue?.Description))
+                    NotificationActionService.TriggerAction(actionValue.Description);
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+            }
+        }
+
+        public override void RegisteredForRemoteNotifications(
+            UIApplication application,
+            NSData deviceToken)
+            => CompleteRegistrationAsync(deviceToken).ContinueWith((task)
+                => { if (task.IsFaulted) throw task.Exception; });
+
+        public override void ReceivedRemoteNotification(
+            UIApplication application,
+            NSDictionary userInfo)
+            => ProcessNotificationActions(userInfo);
+
+        public override void FailedToRegisterForRemoteNotifications(
+            UIApplication application,
+            NSError error)
+            => Debug.WriteLine(error.Description);
+
         //
         // This method is invoked when the application has loaded and is ready to run. In this 
         // method you should instantiate the window, load the UI into it and then make the window
@@ -23,7 +106,23 @@ namespace PushTest.iOS
         public override bool FinishedLaunching(UIApplication app, NSDictionary options)
         {
             global::Xamarin.Forms.Forms.Init();
+            Bootstrap.Begin(() => new DeviceInstallationService());
+            if (DeviceInstallationService.NotificationsSupported)
+            {
+                UNUserNotificationCenter.Current.RequestAuthorization(
+                        UNAuthorizationOptions.Alert |
+                        UNAuthorizationOptions.Badge |
+                        UNAuthorizationOptions.Sound,
+                        (approvalGranted, error) =>
+                        {
+                            if (approvalGranted && error == null)
+                                RegisterForRemoteNotifications();
+                        });
+            }
             LoadApplication(new App());
+            using (var userInfo = options?.ObjectForKey(
+                UIApplication.LaunchOptionsRemoteNotificationKey) as NSDictionary)
+                            ProcessNotificationActions(userInfo);
 
             return base.FinishedLaunching(app, options);
         }
